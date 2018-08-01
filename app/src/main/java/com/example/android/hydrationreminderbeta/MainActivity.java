a190f88ee109774a976922163ed7c76614c6b372
Smarpit Singh <smarpitsingh560@gmail.com>
2018-08-01 20:23:00 +0530

    intentService Added

--- app/src/main/java/com/example/android/hydrationreminderbeta/MainActivity.java
+++ app/src/main/java/com/example/android/hydrationreminderbeta/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.hydrationreminderbeta;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -9,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.hydrationreminderbeta.sync.ReminderTasks;
+import com.example.android.hydrationreminderbeta.sync.WaterReminderIntentService;
 import com.example.android.hydrationreminderbeta.utilities.PreferenceUtilities;
 
 public class MainActivity extends AppCompatActivity implements
@@ -25,31 +28,25 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /** Get the views **/
+
         mWaterCountDisplay = (TextView) findViewById(R.id.tv_water_count);
         mChargingCountDisplay = (TextView) findViewById(R.id.tv_charging_reminder_count);
         mChargingImageView = (ImageView) findViewById(R.id.iv_power_increment);
 
-        /** Set the original values in the UI **/
         updateWaterCount();
         updateChargingReminderCount();
 
-        /** Setup the shared preference listener **/
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
-    /**
-     * Updates the TextView to display the new water count from SharedPreferences
-     */
+
     private void updateWaterCount() {
         int waterCount = PreferenceUtilities.getWaterCount(this);
         mWaterCountDisplay.setText(waterCount+"");
     }
 
-    /**
-     * Updates the TextView to display the new charging reminder count from SharedPreferences
-     */
+
     private void updateChargingReminderCount() {
         int chargingReminders = PreferenceUtilities.getChargingReminderCount(this);
         String formattedChargingReminders = getResources().getQuantityString(
@@ -58,30 +55,26 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
-    /**
-     * Adds one to the water count and shows a toast
-     */
+
     public void incrementWater(View view) {
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
-        // TODO (15) Create an explicit intent for WaterReminderIntentService
-        // TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
-        // TODO (17) Call startService and pass the explicit intent you just created
+
+        Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
+        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+        startService(incrementWaterCountIntent);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        /** Cleanup the shared preference listener **/
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.unregisterOnSharedPreferenceChangeListener(this);
     }
 
-    /**
-     * This is a listener that will update the UI when the water count or charging reminder counts
-     * change
-     */
+   
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (PreferenceUtilities.KEY_WATER_COUNT.equals(key)) {
--- app/src/main/java/com/example/android/hydrationreminderbeta/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/hydrationreminderbeta/sync/ReminderTasks.java
@@ -1,14 +1,23 @@
 package com.example.android.hydrationreminderbeta.sync;
 
-// TODO (1) Create a class called ReminderTasks
+import android.content.Context;
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+import com.example.android.hydrationreminderbeta.utilities.PreferenceUtilities;
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+public class ReminderTasks {
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+    public static void executeTask(Context context, String action) {
+
+        if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
+            incrementWaterCount(context);
+        }
+    }
+
+    private static void incrementWaterCount(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+}
+
